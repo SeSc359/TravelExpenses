e569d4960b4601ac090b8716c1c5261fa3b87cfc
Yinjue <friedaqian@hotmail.com>
2019-08-20 15:33:43 +0200

    ngif

--- Frontend/ReisenKostenAppFrontend/src/app/status/status.component.ts
+++ Frontend/ReisenKostenAppFrontend/src/app/status/status.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status.component.css']
 })
 export class StatusComponent implements OnInit {
-  // expenselist:Expense[];
+  
 
   statuscolor='red';
   travelExpenseList: TravelExpense[];
