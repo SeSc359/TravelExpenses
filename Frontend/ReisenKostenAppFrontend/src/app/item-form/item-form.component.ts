95fe70902530f56514ba20ccef34c57f1d78f973
Yinjue <friedaqian@hotmail.com>
2019-08-23 10:19:44 +0200

    upload

--- Frontend/ReisenKostenAppFrontend/src/app/item-form/item-form.component.ts
+++ Frontend/ReisenKostenAppFrontend/src/app/item-form/item-form.component.ts
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import { TrexItem } from './../Entity/TrexItem';
-=======
-import { Item } from '../Entity/Item';
-import { TravelExpenseService } from './../travel-expense.service';
 
-
->>>>>>> features_lukas
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { TravelExpenseService } from '../travel-expense.service';
+import { Item } from '../Entity/Item';
 
 @Component({
   selector: 'app-item-form',
@@ -18,9 +12,14 @@ import { TravelExpenseService } from '../travel-expense.service';
 export class ItemFormComponent implements OnInit {
 
   itemForm: FormGroup;
-  trexItemList: TrexItem[];
-  trexItem : TrexItem
+  ItemList: Item[];
+  Item : Item
   isCreating: boolean = false;
+  
+  
+  files: File[];
+  file: File;
+  error: string ="Not Funded"
 
   constructor(private fb:FormBuilder,private travelExpenseService: TravelExpenseService) { }
   ngOnInit() {
@@ -38,38 +37,22 @@ export class ItemFormComponent implements OnInit {
     this.isCreating = true;
   }
 
-<<<<<<< HEAD
-  createTrexItem(){
-    const trexItem: TrexItem = this.itemForm.value;
-    // trexItem.id = null;
-    this.travelExpenseService.createTrexItem(trexItem).subscribe(travelItem => this.trexItemList.push(trexItem));
-=======
-  createItem(){
+   createItem(){
     const Item: Item = this.itemForm.value;
     Item.id = null;
     this.travelExpenseService.createItem(Item).subscribe(Item => this.ItemList.push(Item));
->>>>>>> features_lukas
    }
-   files: File[];
-  
-   file: File;
-  
-   error: string
- 
    
- 
-   handleFileInput(files: FileList) {
-      this.file = files.item(0);
-  }
+  FileSelect(){
+  this.travelExpenseService.onFileSelected(event);
+}
  
    uploadFileToActivity() {
-     
-    this.travelExpenseService.createAttachment(this.file).subscribe((response) => {
-       console.log('response', response)
-     }, error => {
-       console.log(error);
-     });
+     this.travelExpenseService.onUpload();
+    
  }
+
+
 
    
 
